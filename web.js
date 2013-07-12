4b0bfe1116554405e1ea115fe324df2f8a96c9cc
Jbotaldrich <jbot.aldrich@gmail.com>
2013-07-12 06:09:36 +0000

    Looks at the file now

--- web.js
+++ web.js
@@ -4,8 +4,9 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   fs.readFileSync("index.html", function(err, data){
-  if(err) throw err;
+
   response.send(data);
+});
 });
 
 var port = process.env.PORT || 5000;
